238725c11a22e9e89ff8318e46437adf0ea27c70
Aly Ashour <alyashour1@gmail.com>
2025-11-10 16:33:40 -0500

    Added ap1_msgs dependency and updated topic paths

--- include/ap1/control/control_node.hpp
+++ include/ap1/control/control_node.hpp
@@ -12,6 +12,9 @@
 #include "std_msgs/msg/float32.hpp"
 #include "std_msgs/msg/float32_multi_array.hpp"
 
+#include "ap1_msgs/msg/speed_profile_stamped.hpp"
+#include "ap1_msgs/msg/target_path_stamped.hpp"
+
 #include "ap1/control/controller.hpp"
 #include "ap1/control/pd_controller.hpp"
 
@@ -27,22 +30,22 @@ private:
   // Memory
 
   // Subs
-  rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr
-      speed_profile_sub_;
-  rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr
+  rclcpp::Subscription<ap1_msgs::msg::TargetPathStamped>::SharedPtr
       target_path_sub_;
+  rclcpp::Subscription<ap1_msgs::msg::SpeedProfileStamped>::SharedPtr
+      speed_profile_sub_;
 
   // Pubs
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr turning_angle_pub_;
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr
       motor_power_pub_; // between -1 and 1? probably
 
-  void on_speed_profile(const std_msgs::msg::Float32MultiArray::SharedPtr) {
+  void on_speed_profile(const ap1_msgs::msg::SpeedProfileStamped) {
     // todo: implement
     RCLCPP_INFO(this->get_logger(), "Received Speed Profile from Planning");
   }
 
-  void on_path(const std_msgs::msg::Float32MultiArray::SharedPtr) {
+  void on_path(const ap1_msgs::msg::TargetPathStamped) {
     // todo: implement
     RCLCPP_INFO(this->get_logger(), "Received new path from Planning");
   }
@@ -54,23 +57,23 @@ public:
     // # All inputs shabooya
     // - SPEED PROFILE
     speed_profile_sub_ =
-        this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "speed_profile", 10,
+        this->create_subscription<ap1_msgs::msg::SpeedProfileStamped>(
+            "ap1/planning/speed_profile", 10,
             std::bind(&ControlNode::on_speed_profile, this,
                       std::placeholders::_1));
     // - TARGET PATH
     target_path_sub_ =
-        this->create_subscription<std_msgs::msg::Float32MultiArray>(
-            "target_path", 10,
+        this->create_subscription<ap1_msgs::msg::TargetPathStamped>(
+            "ap1/planning/speed_profile", 10,
             std::bind(&ControlNode::on_path, this, std::placeholders::_1));
 
     // # Publishers
     // - TURNING ANGLE
-    turning_angle_pub_ =
-        this->create_publisher<std_msgs::msg::Float32>("turning_angle", 10);
+    turning_angle_pub_ = this->create_publisher<std_msgs::msg::Float32>(
+        "ap1/control/motor_power", 10);
     // - MOTOR POWER
-    motor_power_pub_ =
-        this->create_publisher<std_msgs::msg::Float32>("motor_power", 10);
+    motor_power_pub_ = this->create_publisher<std_msgs::msg::Float32>(
+        "ap1/control/turn_angle", 10);
 
     RCLCPP_INFO(this->get_logger(), "Control Node initialized");
   }
