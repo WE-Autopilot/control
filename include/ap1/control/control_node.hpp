e6546c22c0444e25c0c9b6a76141d0f7750d20a8
Aly Ashour <alyashour1@gmail.com>
2025-11-10 17:26:14 -0500

    Updated topic locations and message types according to pciv3. Needs ackermann to convert from acceleration to power/steer commands

--- include/ap1/control/control_node.hpp
+++ include/ap1/control/control_node.hpp
@@ -6,14 +6,13 @@
 #ifndef AP1_CONTROL_NODE_HPP
 #define AP1_CONTROL_NODE_HPP
 
-#include "geometry_msgs/msg/point.hpp"
-#include "geometry_msgs/msg/point32.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/float32.hpp"
-#include "std_msgs/msg/float32_multi_array.hpp"
 
+#include "ap1_msgs/msg/motor_power_stamped.hpp"
 #include "ap1_msgs/msg/speed_profile_stamped.hpp"
 #include "ap1_msgs/msg/target_path_stamped.hpp"
+#include "ap1_msgs/msg/turn_angle_stamped.hpp"
+#include "ap1_msgs/msg/vehicle_speed_stamped.hpp"
 
 #include "ap1/control/controller.hpp"
 #include "ap1/control/pd_controller.hpp"
@@ -23,35 +22,74 @@ class ControlNode : public rclcpp::Node {
 private:
   // Fields
 
+  // # Create Control Loop
+  // fire at rate_hz
+  const double rate_hz_;
+  rclcpp::TimerBase::SharedPtr timer_;
+
   // Controller
   // this should be a sharedptr or something NOT raw.
   ap1::control::IController *controller_;
 
   // Memory
+  // half these types are very unnecessary, we should just have stampedfloat or
+  // stamped double or something
+  ap1_msgs::msg::SpeedProfileStamped speed_profile_;
+  ap1_msgs::msg::TargetPathStamped target_path_;
+  ap1_msgs::msg::VehicleSpeedStamped vehicle_speed_;
+  ap1_msgs::msg::TurnAngleStamped vehicle_turn_angle;
+
+  // target speed
 
   // Subs
   rclcpp::Subscription<ap1_msgs::msg::TargetPathStamped>::SharedPtr
       target_path_sub_;
   rclcpp::Subscription<ap1_msgs::msg::SpeedProfileStamped>::SharedPtr
       speed_profile_sub_;
+  rclcpp::Subscription<ap1_msgs::msg::VehicleSpeedStamped>::SharedPtr
+      vehicle_speed_sub_;
+  rclcpp::Subscription<ap1_msgs::msg::TurnAngleStamped>::SharedPtr
+      vehicle_turn_angle_sub_;
 
   // Pubs
-  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr turning_angle_pub_;
-  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr
+  rclcpp::Publisher<ap1_msgs::msg::TurnAngleStamped>::SharedPtr
+      turning_angle_pub_;
+  rclcpp::Publisher<ap1_msgs::msg::MotorPowerStamped>::SharedPtr
       motor_power_pub_; // between -1 and 1? probably
 
-  void on_speed_profile(const ap1_msgs::msg::SpeedProfileStamped) {
-    // todo: implement
-    RCLCPP_INFO(this->get_logger(), "Received Speed Profile from Planning");
+  void
+  on_speed_profile(const ap1_msgs::msg::SpeedProfileStamped speed_profile) {
+    speed_profile_ = speed_profile;
   }
 
-  void on_path(const ap1_msgs::msg::TargetPathStamped) {
-    // todo: implement
-    RCLCPP_INFO(this->get_logger(), "Received new path from Planning");
+  void on_path(const ap1_msgs::msg::TargetPathStamped target_path) {
+    target_path_ = target_path;
+  }
+
+  void on_speed(const ap1_msgs::msg::VehicleSpeedStamped speed) {
+    vehicle_speed_ = speed;
+  }
+
+  void on_turn_angle(const ap1_msgs::msg::TurnAngleStamped turn_angle) {
+    vehicle_turn_angle = turn_angle;
+  }
+
+  void control_loop_callback() {
+    const std::vector<float> velocity{this->vehicle_speed_.speed, 0,
+                                      0}; // which direction is up? assuming +x
+
+    const std::vector<float> acc = controller_->compute_acceleration(
+        velocity,
+        target_path_.path.at(0),    // for now get the first path waypoint
+        speed_profile_.speeds.at(0) // and the first speed value
+    );
+
+    // send the acceleration through publisher topics
+    // we need to implement ackermann steering here.
   }
 
 public:
-  ControlNode() : Node("control_node") {
+  ControlNode(float rate_hz = 60) : Node("control_node"), rate_hz_(rate_hz) {
     controller_ = new ap1::control::PDController();
 
     // # All inputs shabooya
@@ -66,14 +104,30 @@ public:
         this->create_subscription<ap1_msgs::msg::TargetPathStamped>(
             "ap1/planning/speed_profile", 10,
             std::bind(&ControlNode::on_path, this, std::placeholders::_1));
+    vehicle_speed_sub_ =
+        this->create_subscription<ap1_msgs::msg::VehicleSpeedStamped>(
+            "ap1/actuation/speed_actual", 10,
+            std::bind(&ControlNode::on_speed, this, std::placeholders::_1));
+    vehicle_turn_angle_sub_ =
+        this->create_subscription<ap1_msgs::msg::TurnAngleStamped>(
+            "ap1/actuation/turn_angle_actual", 10,
+            std::bind(&ControlNode::on_turn_angle, this,
+                      std::placeholders::_1));
 
     // # Publishers
     // - TURNING ANGLE
-    turning_angle_pub_ = this->create_publisher<std_msgs::msg::Float32>(
-        "ap1/control/motor_power", 10);
+    turning_angle_pub_ =
+        this->create_publisher<ap1_msgs::msg::TurnAngleStamped>(
+            "ap1/control/motor_power", 10);
     // - MOTOR POWER
-    motor_power_pub_ = this->create_publisher<std_msgs::msg::Float32>(
+    motor_power_pub_ = this->create_publisher<ap1_msgs::msg::MotorPowerStamped>(
         "ap1/control/turn_angle", 10);
+
+    // # Create Control Loop
+    // fire at rate_hz
+    timer_ = this->create_wall_timer(
+        std::chrono::duration<double>(1.0 / rate_hz),
+        std::bind(&ControlNode::control_loop_callback, this));
 
     RCLCPP_INFO(this->get_logger(), "Control Node initialized");
   }
