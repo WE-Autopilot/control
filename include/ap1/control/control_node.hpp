89b246f61165e84c1e66124020d361166d815e73
cadence-m <cmcgil3@uwo.ca>
2025-12-03 10:39:09 -0500

    consolidated float messages into single type

--- include/ap1/control/control_node.hpp
+++ include/ap1/control/control_node.hpp
@@ -11,11 +11,8 @@
 
 #include "rclcpp/rclcpp.hpp"
 
-#include "ap1_msgs/msg/motor_power_stamped.hpp"
 #include "ap1_msgs/msg/speed_profile_stamped.hpp"
 #include "ap1_msgs/msg/target_path_stamped.hpp"
-#include "ap1_msgs/msg/turn_angle_stamped.hpp"
-#include "ap1_msgs/msg/vehicle_speed_stamped.hpp"
 
 #include "ap1/control/ackermann_controller.hpp"
 #include "ap1/control/icontroller.hpp"
