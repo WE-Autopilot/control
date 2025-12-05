1802c65f35b9ae08a2433946fc3c50223dd02909
michaelwestern1 <mpachows@uwo.ca>
2025-12-05 19:42:30 +0000

    Address review comments: clean headers, implement reset, fix package.xml

--- include/ap1/control/control_node.hpp
+++ include/ap1/control/control_node.hpp
@@ -15,17 +15,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include "yaml-cpp/yaml.h"
 
-// ---- AP1 Messages ----
 #include "ap1_msgs/msg/motor_power_stamped.hpp"
 #include "ap1_msgs/msg/speed_profile_stamped.hpp"
 #include "ap1_msgs/msg/target_path_stamped.hpp"
 #include "ap1_msgs/msg/turn_angle_stamped.hpp"
 #include "ap1_msgs/msg/vehicle_speed_stamped.hpp"
 
-// ---- Your Reset Service ----
 #include "std_srvs/srv/trigger.hpp"
 
-// ---- Controllers (upstream) ----
 #include "ap1/control/ackermann_controller.hpp"
 #include "ap1/control/icontroller.hpp"
 
@@ -39,33 +36,19 @@ public:
     ControlNode(const std::string &cfg_path, float rate_hz = 60);
 
 private:
-    // --------------------------------------------------------------------
-    // CONFIG + INTERNAL STATE
-    // --------------------------------------------------------------------
     void load_config();
 
     std::string config_path_;
     int rate_hz_{60};
 
-    float last_velocity_ = 0.0f;
-    float last_error_ = 0.0f;
-    float integral_term_ = 0.0f;
-
-    // --------------------------------------------------------------------
-    // UPSTREAM CONTROLLER ARCHITECTURE
-    // --------------------------------------------------------------------
     std::unique_ptr<IController> controller_;
     AckermannController ackermann_controller_;
 
-    // Cached “latest values”
     ap1_msgs::msg::SpeedProfileStamped speed_profile_;
     ap1_msgs::msg::TargetPathStamped target_path_;
     ap1_msgs::msg::VehicleSpeedStamped vehicle_speed_;
     ap1_msgs::msg::TurnAngleStamped vehicle_turn_angle_;
 
-    // --------------------------------------------------------------------
-    // SUBSCRIPTIONS
-    // --------------------------------------------------------------------
     rclcpp::Subscription<ap1_msgs::msg::TargetPathStamped>::SharedPtr target_path_sub_;
     rclcpp::Subscription<ap1_msgs::msg::SpeedProfileStamped>::SharedPtr speed_profile_sub_;
     rclcpp::Subscription<ap1_msgs::msg::VehicleSpeedStamped>::SharedPtr vehicle_speed_sub_;
@@ -76,21 +59,14 @@ private:
     void on_speed(const ap1_msgs::msg::VehicleSpeedStamped msg);
     void on_turn_angle(const ap1_msgs::msg::TurnAngleStamped msg);
 
-    // --------------------------------------------------------------------
-    // PUBLISHERS
-    // --------------------------------------------------------------------
+
     rclcpp::Publisher<ap1_msgs::msg::TurnAngleStamped>::SharedPtr turning_angle_pub_;
     rclcpp::Publisher<ap1_msgs::msg::MotorPowerStamped>::SharedPtr motor_power_pub_;
 
-    // --------------------------------------------------------------------
-    // CONTROL LOOP
-    // --------------------------------------------------------------------
     rclcpp::TimerBase::SharedPtr timer_;
     void control_loop_callback();
 
-    // --------------------------------------------------------------------
-    // RESET SERVICE
-    // --------------------------------------------------------------------
+ 
     rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr reset_service_;
 
     void handle_reset(
